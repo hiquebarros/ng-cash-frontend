4011d2aa234ca3dc38ef49f699c7f7145fb81e35
Henrique Barros <henrique1_barros@hotmail.com>
2022-11-17 12:28:51 -0300

    feat: header

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const StyledHeader = styled.header`
+display: flex;
+justify-content: space-between;
+align-items: center;
+padding: 0px 1em;
+background-color: var(--black);
+width: 100%;
+height: 80px;
+div{
+    display: flex;
+    justify-content: center;
+    width: 120px;
+}
+img{
+    width: 100%;
+}
+`
